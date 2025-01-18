f3e587abf74647cd636bb875f70d5cb06dbf3dba
Shlok2004 <shlok.kp22@gmail.com>
2025-01-18 14:15:59 -0500

    ver 1.2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,12 +7,15 @@ export default function Page() {
  <div>
   <main className={styles.main}>
   <p>
-    About Page
+    Welcome to the page all about me!
+  </p>
+  <p>
+    I am a Junior at Rutgers University New Brunswick majoring in Computer Science B.S. 
   </p>
   
  
    
-
+  <a href="/ " className={styles.topLink}>Back</a>
 
 
 </main>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import styles from "./page.module.css";
 
 export default function Page() {
-    return <p>About Page</p>;
+  
+    return(
+ <div>
+  <main className={styles.main}>
+  <p>
+    LinkedIn : <a href="https://www.linkedin.com/in/shlokparekh/" className={styles.topLink} target="_blank" rel="noopener noreferrer">
+                    <h2>LinkedIn <span>-&gt;</span></h2>
+                    <p>My LinkedIn Page!</p>
+                  </a>
+    Email : shlok.kp22@gmail.com
+    Phone : 925-549-4470
+  </p>
+  
+ 
+   
+  <a href="/ " className={styles.topLink}>Back</a>
+
+
+</main>
+    </div>
+    
+
+
+
+
+
+
+    );
   }
